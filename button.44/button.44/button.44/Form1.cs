4144b36585d0e2dfc23f217e3c2408e4a3f6ff69
11meryem <145452149+11meryem@users.noreply.github.com>
2023-11-27 09:57:00 +0300

    Add files via upload

---
+++ button.44/button.44/button.44/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button._44
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hasta hasta = new Hasta();
+            hasta.ad = "Büşra";
+            hasta.soyad = "Ekmen";
+            hasta.hastaligi = "Şizofren";
+            hasta.yas = 61;
+            hasta.tc = 15384603685;
+            hasta.odaNum = 5;
+            hasta.bakiciAdi = "Meryem Hanım";
+
+            label1.Text=hasta.ad+"  "+hasta.soyad+"  "+hasta.hastaligi+"  "+hasta.yas+"  "+hasta.tc+"  "+hasta.odaNum+"  "+hasta.bakiciAdi+"  ";
+        }
+    }
+}
---
+++ button.44/button.44/button.44/Hasta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace button._44
+{
+    internal class Hasta
+    {
+        public string ad;
+        public string soyad;
+        public int yas;
+        public double tc;
+        public int odaNum;
+        public string bakiciAdi;
+        public string hastaligi;
+
+    }
+}
